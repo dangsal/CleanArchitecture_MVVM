8c4f23b217b67738a252cf6f49c3176c1022ba50
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-24 23:51:41 +0900

    [ADD] 셀 생성

--- CleanArchitecture_MVVM/CleanArchitecture_MVVM/Presentation/Scene/News/Cell/ArticleCell.swift
+++ CleanArchitecture_MVVM/CleanArchitecture_MVVM/Presentation/Scene/News/Cell/ArticleCell.swift
@@ -5,4 +5,62 @@
 //  Created by 이성호 on 2023/09/24.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+
+final class ArticleCell: UITableViewCell {
+    
+    // MARK: - ui component
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "title"
+        label.font = .boldSystemFont(ofSize: 22)
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "description"
+        label.font = .systemFont(ofSize: 15)
+        return label
+    }()
+    
+    // MARK: - init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setupLayout()
+        self.configureUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - func
+    
+    private func setupLayout() {
+        self.addSubview(self.titleLabel)
+        self.titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.leading.equalTo(self.snp.leading).inset(20)
+            $0.trailing.equalTo(self.snp.trailing).inset(20)
+        }
+        
+        self.addSubview(self.descriptionLabel)
+        self.descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(10)
+        }
+    }
+    
+    private func configureUI() {
+        
+    }
+}
--- CleanArchitecture_MVVM/CleanArchitecture_MVVM/Presentation/Scene/News/View/NewsView.swift
+++ CleanArchitecture_MVVM/CleanArchitecture_MVVM/Presentation/Scene/News/View/NewsView.swift
@@ -7,9 +7,17 @@
 
 import UIKit
 
+import SnapKit
+
 final class NewsView: UIView, BaseViewType {
     
     // MARK: - ui component
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(ArticleCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
     
     // MARK: - property
     
@@ -18,6 +26,7 @@ final class NewsView: UIView, BaseViewType {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.baseInit()
+        self.setupDelegate()
     }
     
     @available(*, unavailable)
@@ -27,14 +36,51 @@ final class NewsView: UIView, BaseViewType {
     
     // MARK: - func
     
-}
-
-extension NewsView {
+    private func setupDelegate() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
     func setupLayout() {
-        
+        self.addSubview(self.tableView)
+        self.tableView.snp.makeConstraints {
+            $0.top.equalTo(self.snp.top)
+            $0.leading.equalTo(self.snp.leading)
+            $0.trailing.equalTo(self.snp.trailing)
+            $0.bottom.equalTo(self.snp.bottom)
+        }
     }
     
     func configureUI() {
         
     }
 }
+
+extension NewsView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ArticleCell else { return UITableViewCell() }
+        return cell
+    }
+}
+
+extension NewsView: UITableViewDelegate { }
+
+
+
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+struct CellPreview: PreviewProvider{
+    static var previews: some View {
+        UIViewPreview {
+            let view = NewsView()
+            return view
+        }.previewLayout(.sizeThatFits)
+    }
+}
+#endif
