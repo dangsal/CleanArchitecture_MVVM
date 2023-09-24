1325644ac0b7823f855199de80e5686b9e2fa9e3
LEE SUNG HO <59243274+dangsal@users.noreply.github.com>
2023-09-24 22:51:21 +0900

    [FEAT] BaseViewType 프로토콜 생성

---
+++ CleanArchitecture_MVVM/CleanArchitecture_MVVM/Presentation/Common/Base/BaseViewType.swift
@@ -0,0 +1,25 @@
+//
+//  BaseViewType.swift
+//  CleanArchitecture_MVVM
+//
+//  Created by 이성호 on 2023/09/24.
+//
+
+import UIKit
+
+///
+/// UIView 타입의 클래스를 구성하기 위한 기본적인 함수를 제공합니다.
+///
+
+protocol BaseViewType: UIView {
+    func setupLayout()
+    func configureUI()
+}
+
+extension BaseViewType {
+    /// 기본적인 메서드를 한 번에 호출하는 메서드
+    func baseInit() {
+        self.setupLayout()
+        self.configureUI()
+    }
+}
